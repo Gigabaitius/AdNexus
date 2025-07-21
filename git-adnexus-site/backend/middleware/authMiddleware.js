673c0d6131c87bf566bc5d2b81f41e678fc43fd0
Gigabaitius <gigabaitius0@gmail.com>
2025-07-21 09:25:24 +0300

    Add files via upload

---
+++ git-adnexus-site/backend/middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+// Middleware - промежуточные обработчики запросов (аутентификация, логирование, и т.д.)
+
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = process.env.SECRET_KEY; // тот же ключ, что и при генерации токена
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"]; // Заголовок Authorization
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1]; // Берем вторую часть "Bearer <token>"
+  if (!token) {
+    return res.status(401).json({ message: "Malformed token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY); // Расшифровываем токен
+    req.user = decoded; // Передаем в следующий обработчик
+    next(); // Пропускаем дальше
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+}
+
+function requireAdmin(req, res, next) {
+  if (!req.user || req.user.is_admin !== 1) {
+    return res.status(403).json({ message: "Access denied: Admins only." });
+  }
+  next(); // допуск, если admin
+}
+
+function requireModerator(req, res, next) {
+  if (!req.user || (req.user.is_moderator !== 1 && req.user.is_admin !== 1)) {
+    return res.status(403).json({ message: "Access denied: Moderators only." });
+  }
+  next(); // допуск, если moder или admin
+}
+
+module.exports = { authMiddleware, requireAdmin, requireModerator };
---
+++ git-adnexus-site/backend/validators/campaignValidator.js
@@ -0,0 +1,31 @@
+// пробить npm install joi
+
+const Joi = require('joi');
+
+// Схема для создания/обновления кампании
+const campaignSchema = Joi.object({
+  title: Joi.string().min(3).max(100).required(),
+  description: Joi.string().allow('').optional(),
+  budget: Joi.number().positive().required(),
+  start_date: Joi.date().iso().required(),
+  end_date: Joi.date().iso().min(Joi.ref('start_date')).allow(null).optional(),
+  status: Joi.string().valid('pending', 'active', 'paused', 'completed').default('pending')
+});
+
+// Middleware для валидации
+const validateCampaign = (req, res, next) => {
+  const { error } = campaignSchema.validate(req.body);
+  
+  if (error) {
+    return res.status(400).json({
+      success: false,
+      error: {
+        message: error.details[0].message
+      }
+    });
+  }
+  
+  next();
+};
+
+module.exports = { validateCampaign };
